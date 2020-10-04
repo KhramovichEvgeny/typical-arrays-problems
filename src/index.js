f1762795f4e95d5a3297689cf17b97be4b1e2ba9
Khramovich Evgeny <nupker@mail.ru>
2020-10-04 15:08:34 +0300

    feat: solve all problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0;
+  let minimal = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if(minimal > array[i]) {
+      minimal = array[i];
+    }
+  }
+  return minimal;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0;
+  let maximal = array[0];
+  for (let i = 0; i < array.length; i++) {
+    if(maximal <= array[i]) {
+      maximal = array[i];
+    }
+  }
+  return maximal;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array === undefined || array[0] === undefined) return 0;
+  let sum = 0;
+  for (let i = 0; i < array.length; i++) {
+    sum += array[i];
+    }
+  return sum / array.length;
 }
